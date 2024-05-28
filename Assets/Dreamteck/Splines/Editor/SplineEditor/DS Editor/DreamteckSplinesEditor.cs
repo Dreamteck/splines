a4100d4e08853e9f7138edccd5d06f213abdc620
Kronoxis <dev.kronoxis@gmail.com>
2024-05-28 13:03:28 +0200

    Optimize disconnect `DisconnectNode` runs on an index and loops through the nodes (doing nothing if the node at index doesn't exist), making it pointless to check if the node exists before calling the method.

--- Assets/Dreamteck/Splines/Editor/SplineEditor/DS Editor/DreamteckSplinesEditor.cs
+++ Assets/Dreamteck/Splines/Editor/SplineEditor/DS Editor/DreamteckSplinesEditor.cs
@@ -291,11 +291,8 @@ namespace Dreamteck.Splines.Editor
                     spline.DisconnectNode(node.Key);
                     nodes.Add(node.Key - 1, node.Value);
                 }
-                if (node.Key == index)
-                {
-                    spline.DisconnectNode(node.Key);
-                }
             }
+            spline.DisconnectNode(index);
             var nodesProperty = _serializedObject.FindProperty("_nodes");
             for (int i = 0; i < nodesProperty.arraySize; i++)
             {
